0ca29e2627637850353df8bc4c0f633b51e9ee70
Gustavo Rocha <gustavo.prietorocha1904@gmail.com>
2023-05-02 00:29:02 -0300

    UPDATE gitignore & first working version
    Added the authenticaton JSON files to gitignore. Program can search a file and add songs to a created playlist

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ def yt():
     if not os.path.exists('headers_auth.json'):
         YTMusic.setup(filepath="headers_auth.json")
     
-    ytmusic = YTMusic('headers_auth.json')
+    ytmusic = YTMusic('browser.json')
     playlist_name = 'Teste'
-    playlist_id = ytmusic.create_playlist(playlist_name, description='',privacy_status="PRIVATE")['playlist_id']
-    search_results = ytmusic.search('Naquela Mesa')
+    playlist_id = ytmusic.create_playlist(playlist_name, '',privacy_status="PRIVATE")
+    with open('songs.txt','r',encoding='utf-8') as file:
+        for line in file:
+            search_results = ytmusic.search(line ,filter='songs')
+            if search_results:
+                song = search_results[0]
+                video_id = song['videoId']
+                ytmusic.add_playlist_items(playlist_id, [search_results[0]['videoId']])
+    
     #Creation of a Playlist
     #YTMusic.get_artist(channelId='UCrPe3hLA51968GwxHSZ1llw')
     #print('Name playlist for modification:')
